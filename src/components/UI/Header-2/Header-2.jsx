71cc565fd8fc710498920af18580af9bfe40514d
crystalgem1337 <man2123@mail.ru>
2022-09-22 23:36:16 +0300

    Log in / out added

--- src/components/UI/Header-2/Header-2.jsx
+++ src/components/UI/Header-2/Header-2.jsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import cl from "./Header-2.module.css";
 import Icon from "../iconELem/Icon";
+import classes from "classnames";
 
 
 const Header2 = () => {
+    const [active, setActive] = useState(false);
+
+    useEffect(() => {
+        console.log("pzdc - ", active)
+    }, [active])
+
     return (
         <div className={cl.header2}>
             <ul className={cl.linksArea}>
@@ -26,7 +33,13 @@ const Header2 = () => {
                 <li>Consultation</li>
             </ul>
 
-            <Icon color={"white"} transform={1.3}>menu</Icon>
+            <div className={cl.menuIcon}>
+                <Icon color={"white"} transform={1.3} onClick={() => setActive(!active)}>menu</Icon>
+                <div className={active ? cl.menu : classes(cl.menu, cl.invisible)}>
+                    <div className={active ? cl.logElem : classes(cl.logElem, cl.invisible)}>Sign in</div>
+                    <div className={active ? cl.logElem : classes(cl.logElem, cl.invisible)}>Sign up</div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/components/UI/iconELem/Icon.jsx
+++ src/components/UI/iconELem/Icon.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const Icon = ({children, transform, color}) => {
+const Icon = ({children, transform, color, onClick}) => {
     return (
-        <span className="material-symbols-outlined"
-              style={{transform: `scale(${transform})`, color: color, transition: "all 0.3s ease 0s"}}>
+        <span onClick={onClick} className="material-symbols-outlined"
+              style={{transform: `scale(${transform})`, color: color, transition: "all 0.3s ease 0s", userSelect: "none"}}>
             {children}
         </span>
     );
